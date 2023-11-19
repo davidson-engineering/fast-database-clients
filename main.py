cef550654197fbfa19d2337daea982eaff6ad124
Matthew Davidson <admin@davidson.engineering>
2023-11-19 14:20:44 -0800

    updated

--- main.py
+++ main.py
@@ -8,9 +8,23 @@
 """a_short_project_description"""
 # ---------------------------------------------------------------------------
 
+from fast_influxdb_client.fast_influxdb_client import FastInfluxDBClient, InfluxMetric
+import random
+import time
 
 def main():
-    pass
+    client = FastInfluxDBClient()
+    print(f"{client=}")
+
+    while 1:
+        data = random.random()
+        metric = InfluxMetric(
+            measurement='py_metric1',
+            fields={'data1':data, 'data2':1}
+        )
+        client.write_metric(metric)
+        print(f"{data=}")
+        time.sleep(5)
 
 
 if __name__ == "__main__":

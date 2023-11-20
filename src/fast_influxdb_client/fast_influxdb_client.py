10ed1ee674e5725f15df40e4cfac1f4a54858586
Matthew Davidson <admin@davidson.engineering>
2023-11-19 23:23:41 -0800

    added data type tests

--- src/fast_influxdb_client/fast_influxdb_client.py
+++ src/fast_influxdb_client/fast_influxdb_client.py
@@ -5,7 +5,7 @@
 # Created Date: 2023-03-01
 # version ='1.0'
 # ---------------------------------------------------------------------------
-"""a_short_module_description"""
+"""FastInfluxDBClient is a class to enable rapid deployment of a client to send metrics to InfluxDB server"""
 # ---------------------------------------------------------------------------
 from dataclasses import dataclass, asdict, field
 from datetime import datetime
@@ -15,8 +15,10 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 import logging
 
+
 class ClientEnvVariableNotDefined(Exception):
     pass
+
 
 class InfluxDBWriteError(Exception):
     pass
@@ -29,12 +31,11 @@ class InfluxMetric:
     time: datetime = field(default_factory=datetime.utcnow)
 
     def __repr__(self) -> str:
-        fields = [f"{k}:{v}" for k,v in self.fields]
+        fields = [f"{k}:{v}" for k, v in self.fields]
         return f"{self.__class__.__name__}: {self.measurement}-{fields} at {self.time}"
 
     def as_dict(self) -> dict:
         return asdict(self)
-
 
 
 class FastInfluxDBClient:
@@ -42,15 +43,14 @@ class FastInfluxDBClient:
     A class for sending data to an InfluxDB server using the InfluxDB client API.
     """
 
-    TOKEN_VAR = 'TOKEN'
-    CLIENT_URL_VAR = 'CLIENT_URL'
-    ORG_VAR = 'ORG'
-    BUCKET_VAR = 'BUCKET'
+    TOKEN_VAR = "TOKEN"
+    CLIENT_URL_VAR = "CLIENT_URL"
+    ORG_VAR = "ORG"
+    BUCKET_VAR = "BUCKET"
 
     client = None
 
     def __init__(self, env_filepath=None):
-
         if env_filepath and not os.path.exists(env_filepath):
             raise ClientEnvVariableNotDefined(f"{env_filepath} does not exist")
 
@@ -59,9 +59,16 @@ class FastInfluxDBClient:
         else:
             load_dotenv()
 
-        for var_name in [self.TOKEN_VAR, self.CLIENT_URL_VAR, self.ORG_VAR, self.BUCKET_VAR]:
+        for var_name in [
+            self.TOKEN_VAR,
+            self.CLIENT_URL_VAR,
+            self.ORG_VAR,
+            self.BUCKET_VAR,
+        ]:
             if os.getenv(var_name) is None:
-                raise ClientEnvVariableNotDefined(f"{var_name} is not defined in .env file")
+                raise ClientEnvVariableNotDefined(
+                    f"{var_name} is not defined in .env file"
+                )
 
         token = os.getenv(self.TOKEN_VAR)
         url = os.getenv(self.CLIENT_URL_VAR)
@@ -76,29 +83,27 @@ class FastInfluxDBClient:
     def __enter__(self):
         return self
 
-    # def __exit__(self, exc_type, exc_val, exc_tb):
-    #     self.client.close()
-
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.client.close()
 
     def write_metric(self, metric, write_option=SYNCHRONOUS):
         try:
+            # Check if fields contain invalid types before attempting to write
+            for value in metric.fields.values():
+                if not isinstance(value, (str, int, float, bool, datetime)):
+                    raise ValueError(f"Invalid field type: {type(value)}")
+
             write_api = self.client.write_api(write_option)
-            write_api.write(self.bucket, self.org , metric, write_precision='s')
+            write_api.write(self.bucket, self.org, metric, write_precision="s")
         except Exception as e:
             logging.error(f"Failed to write data to InfluxDB: {e}")
-            raise InfluxDBWriteError(f"Original exception: {e}")
+            raise InfluxDBWriteError(f"Failed to write metric: {e}") from e
 
-    def write_data(self, measurement:str, fields:dict, time = None):
+    def write_data(self, measurement: str, fields: dict, time=None):
         if time is None:
             time = datetime.utcnow()
-        influx_metric = InfluxMetric(
-            measurement=measurement,
-            time=time,
-            fields=fields
-        )
-        #Saving data to InfluxDB
+        influx_metric = InfluxMetric(measurement=measurement, time=time, fields=fields)
+        # Saving data to InfluxDB
         self.write_metric(influx_metric)
 
     def __repr__(self):
--- tests/test_fast_influxdb_client.py
+++ tests/test_fast_influxdb_client.py
@@ -1,11 +1,19 @@
 import pytest
-from datetime import datetime, timedelta
-from fast_influxdb_client.fast_influxdb_client import FastInfluxDBClient, InfluxMetric, ClientEnvVariableNotDefined, InfluxDBWriteError
+from datetime import datetime
+from fast_influxdb_client.fast_influxdb_client import (
+    FastInfluxDBClient,
+    InfluxMetric,
+    ClientEnvVariableNotDefined,
+    InfluxDBWriteError,
+)
+from influxdb_client.client.write_api import SYNCHRONOUS
+
 
 # Define some constants for testing
 TEST_MEASUREMENT = "test_measurement"
 TEST_FIELDS = {"field1": 42, "field2": "value"}
 TEST_TIME = datetime.utcnow()
+
 
 # Mocking the InfluxDB server
 class MockInfluxDBClient:
@@ -20,9 +28,11 @@ class MockInfluxDBClient:
         # Implement the close method behavior if needed
         pass
 
+
 class MockWriteApi:
     def write(self, bucket, org, metric, write_precision):
         pass
+
 
 def test_fast_influxdb_client_init(monkeypatch):
     # Mock the environment variables
@@ -41,14 +51,18 @@ def test_fast_influxdb_client_init(monkeypatch):
         with FastInfluxDBClient(env_filepath="nonexistent_file"):
             pass
 
+
 def test_fast_influxdb_client_write_metric(monkeypatch):
     # Mock the InfluxDB client
     monkeypatch.setattr(FastInfluxDBClient, "client", MockInfluxDBClient())
 
     # Test writing a metric
     client = FastInfluxDBClient()
-    metric = InfluxMetric(measurement=TEST_MEASUREMENT, fields=TEST_FIELDS, time=TEST_TIME)
+    metric = InfluxMetric(
+        measurement=TEST_MEASUREMENT, fields=TEST_FIELDS, time=TEST_TIME
+    )
     client.write_metric(metric)
+
 
 def test_fast_influxdb_client_write_data(monkeypatch):
     # Mock the InfluxDB client
@@ -58,6 +72,7 @@ def test_fast_influxdb_client_write_data(monkeypatch):
     client = FastInfluxDBClient()
     client.write_data(measurement=TEST_MEASUREMENT, fields=TEST_FIELDS, time=TEST_TIME)
 
+
 def test_fast_influxdb_client_write_data_default_time(monkeypatch):
     # Mock the InfluxDB client
     monkeypatch.setattr(FastInfluxDBClient, "client", MockInfluxDBClient())
@@ -66,6 +81,7 @@ def test_fast_influxdb_client_write_data_default_time(monkeypatch):
     client = FastInfluxDBClient()
     client.write_data(measurement=TEST_MEASUREMENT, fields=TEST_FIELDS)
 
+
 def test_fast_influxdb_client_exit(monkeypatch):
     # Mock the InfluxDB client
     monkeypatch.setattr(FastInfluxDBClient, "client", MockInfluxDBClient())
@@ -73,6 +89,7 @@ def test_fast_influxdb_client_exit(monkeypatch):
     # Test __exit__ method
     with FastInfluxDBClient() as client:
         pass  # If __exit__ doesn't raise an exception, the test passes
+
 
 def test_fast_influxdb_client_write_error(monkeypatch):
     # Mock the InfluxDB client to raise an arbitrary exception
@@ -88,9 +105,87 @@ def test_fast_influxdb_client_write_error(monkeypatch):
 
     # Test InfluxDBWriteError exception
     client = FastInfluxDBClient()
-    metric = InfluxMetric(measurement=TEST_MEASUREMENT, fields=TEST_FIELDS, time=TEST_TIME)
-    
+    metric = InfluxMetric(
+        measurement=TEST_MEASUREMENT, fields=TEST_FIELDS, time=TEST_TIME
+    )
+
     with pytest.raises(InfluxDBWriteError):
         client.write_metric(metric)
 
 
+class TestDataTypes:
+    @pytest.fixture
+    def influx_client(self, monkeypatch):
+        # Mock the InfluxDB client
+        monkeypatch.setattr(FastInfluxDBClient, "client", MockInfluxDBClient())
+        return FastInfluxDBClient()
+
+    def test_write_metric_with_string_field(self, influx_client):
+        # Test writing a metric with a string field
+        metric = InfluxMetric(
+            measurement="test_measurement", fields={"field_name": "string_value"}
+        )
+        influx_client.write_metric(metric, write_option=SYNCHRONOUS)
+
+        # Add assertions if needed
+
+    def test_write_metric_with_integer_field(self, influx_client):
+        # Test writing a metric with an integer field
+        metric = InfluxMetric(measurement="test_measurement", fields={"field_name": 42})
+        influx_client.write_metric(metric, write_option=SYNCHRONOUS)
+
+        # Add assertions if needed
+
+    def test_write_metric_with_float_field(self, influx_client):
+        # Test writing a metric with a float field
+        metric = InfluxMetric(
+            measurement="test_measurement", fields={"field_name": 3.14}
+        )
+        influx_client.write_metric(metric, write_option=SYNCHRONOUS)
+
+        # Add assertions if needed
+
+    def test_write_metric_with_boolean_field(self, influx_client):
+        # Test writing a metric with a boolean field
+        metric = InfluxMetric(
+            measurement="test_measurement", fields={"field_name": True}
+        )
+        influx_client.write_metric(metric, write_option=SYNCHRONOUS)
+
+        # Add assertions if needed
+
+    def test_write_metric_with_datetime_field(self, influx_client):
+        # Test writing a metric with a datetime field
+        now = datetime.utcnow()
+        metric = InfluxMetric(
+            measurement="test_measurement", fields={"field_name": now}
+        )
+        influx_client.write_metric(metric, write_option=SYNCHRONOUS)
+
+        # Add assertions if needed
+
+    def test_write_metric_with_multiple_field_types(self, influx_client):
+        # Test writing a metric with fields of different types
+        metric = InfluxMetric(
+            measurement="test_measurement",
+            fields={
+                "string_field": "string_value",
+                "int_field": 42,
+                "float_field": 3.14,
+                "bool_field": True,
+                "datetime_field": datetime.utcnow(),
+            },
+        )
+        influx_client.write_metric(metric, write_option=SYNCHRONOUS)
+
+        # Add assertions if needed
+
+    def test_write_metric_with_invalid_field_type(self, influx_client):
+        # Test writing a metric with an invalid field type
+        metric = InfluxMetric(
+            measurement="test_measurement", fields={"field_name": complex(1, 2)}
+        )
+
+        # Ensure InfluxDBWriteError is raised for invalid field type
+        with pytest.raises(InfluxDBWriteError):
+            influx_client.write_metric(metric, write_option=SYNCHRONOUS)

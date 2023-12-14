18e9acf16ca66405c462ee28c793c20f28dc3169
Matthew Davidson <mjdavidson@pm.me>
2023-12-14 13:54:33 -0800

    up revved

--- src/fast_influxdb_client/__init__.py
+++ src/fast_influxdb_client/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 from fast_influxdb_client.fast_influxdb_client import FastInfluxDBClient
 from fast_influxdb_client.fast_influxdb_client import InfluxMetric

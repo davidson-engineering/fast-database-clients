b1b8cd9464067ef2aea17935fbe4f8bb5b1bd24c
Matthew Davidson <matthew@davidson.engineering>
2024-02-11 13:47:26 -0800

    corrected config paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,8 @@ from fast_database_clients import FastInfluxDBClient
 
 @pytest.fixture
 def fast_influxdb_client():
-    # config_file = "tests/influxdb_testing_config.toml"
-    config_file = "config.toml"
+    config_file = "tests/influxdb_testing_config.toml"
+    # config_file = "config.toml"
     with FastInfluxDBClient.from_config_file(config_file=config_file) as client:
         yield client
         client.close()
